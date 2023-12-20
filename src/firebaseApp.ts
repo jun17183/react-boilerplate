e986b7568b2756a3fb01e7d2db2fe4dbcfe780ac
jun17183 <jun17183@naver.com>
2023-12-20 17:52:51 +0900

    환경변수 수정

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -5,12 +5,12 @@ export let app: FirebaseApp;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJDECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_IREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJDECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // app이 존재할 경우(=firebase가 연결되어 있을 경우) 그대로 불러오기, 그렇지 않다면 새로 생성
